c26ed682dd5fdf45fcd25387b7c54f7e8fca08cc
Kim Borgen <kimatborgen@gmail.com>
2018-10-08 16:45:02 +0200

    Add positiong and resizing off the windows on a single monitor computer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "secondarywindow.h"
 #include <QApplication>
+#include "qdebug.h"
+#include "QScreen"
 
 int main(int argc, char *argv[])
 {
@@ -10,8 +12,26 @@ int main(int argc, char *argv[])
     MainWindow w1;
     SecondaryWindow w2;
 
+    //The following section will place the two windows side by side on the same window.
+    //TODO Check if there is a second monitor and place the two windows on those two instead of on one screen
+    //Gets the screen geometry
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect  screenGeometry = screen->geometry();
+    int height = screenGeometry.height();
+    int width = screenGeometry.width();
+
+    w1.resize(width/2,height/2);
+    w2.resize(width/2,height/2);
+    //Moves the windows so they are centered and positioned correctly
+    w1.move(0,height/4);
+    w2.move(width/2,height/4);
+
     w1.show();
     w2.show();
+
+    // qDebug() << "Height: " << height;
+    // qDebug() << "Width: " << width;
+
 
     return a.exec();
 }
