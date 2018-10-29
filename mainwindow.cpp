8abbd6dcaf91820ef201cebb105a1c8795ff090f
Kim Borgen <kimatborgen@gmail.com>
2018-10-29 20:37:22 +0100

    Replace temp camera with QtAV stream

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,35 +23,16 @@ MainWindow::MainWindow(QWidget *parent) :
     //setAttribute(Qt::WA_PaintOnScreen); // not needed in Qt 5.2 and up
     //setWindowOpacity(0.95);
 
-    m_player = new AVPlayer(ui->verticalLayout_2);
-    m_vo = new VideoOutput(ui->verticalLayout_2);
+    m_player = new AVPlayer(ui->QtAVWindow);
+
+    m_vo = new VideoOutput(ui->QtAVWindow);
     if (!m_vo->widget()) {
        QMessageBox::warning(0, QString::fromLatin1("QtAV error"), tr("Can not create video renderer"));
        return;
     }
     m_player->setRenderer(m_vo);
-    ui->verticalLayout_2->addWidget(m_vo->widget());
+    ui->QtAVWindow->addWidget(m_vo->widget());
     m_player->play("udp://224.0.0.1:9999");
-
-
-    mCamera = new QCamera();
-
-    mCameraViewfinder = new QCameraViewfinder(this);
-
-    mCameraImageCapture = new QCameraImageCapture(mCamera,this);
-
-    mVBoxLayout = new QVBoxLayout();
-
-    mCamera->setViewfinder(mCameraViewfinder);
-
-    mVBoxLayout->addWidget(mCameraViewfinder);
-    mVBoxLayout->setMargin(0);
-
-    ui->camera_col->addLayout(mVBoxLayout);
-    mCamera->start();
-
-
-
 
     valueI = ui->depth_slider->value();
 
