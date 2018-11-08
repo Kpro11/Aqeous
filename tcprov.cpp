f61011c917fbf7fa0f51b6145df9df06057c6769
Kim Borgen <kimatborgen@gmail.com>
2018-11-08 20:21:02 +0100

    Implement winsock: connection to server

--- tcprov.cpp
+++ tcprov.cpp
@@ -6,19 +6,12 @@
 #include <QDebug>
 #include <windows.h>
 #include <winsock2.h>
-
 #include <ws2tcpip.h>
 
+#pragma comment(lib, "Ws2_32.lib")
 
 TcpRov::TcpRov(QObject *parent) : QObject(parent)
 {
-    // TCP connection (no need to change anything here) ===================================================
-    // Initialize Winsock
-    WSADATA wsaData;
-    int wsaError = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (wsaError != 0) {
-        qDebug() << "WSAStartup failed: " << wsaError;
-    }
 
 }
 
@@ -31,7 +24,49 @@ void TcpRov::readTcpData() {
 }
 
 void TcpRov::tcpConnect() {
-    // TODO implement
+
+    // The following code was mostly supplied by sintef, but modified sligthly on our part.
+
+    // Initialize Winsock
+    int iResult; //variable to check for errors
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        qDebug() << "WSAStartup failed: " << iResult;
+    }
+
+    struct addrinfo *result = NULL,
+            *ptr = NULL,
+            hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(IPString.c_str(), PortString.c_str(), &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
+        WSACleanup();
+    }
+
+    // Attempt to connect to the first address returned by the call to getaddrinfo
+    ptr = result;
+
+    // Create a SOCKET for connecting to server
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+    }
+
+    winsockConnect(&ConnectSocket, ptr);
+
+    // Should really try the next address returned by getaddrinfo if the connect call failed
+    // But for this simple example we just free the resources returned by getaddrinfo and print an error message
+    freeaddrinfo(result);
 }
 
 void TcpRov::tcpSend() {
@@ -55,4 +90,19 @@ void TcpRov::setValues(quint64 north, quint64 east, quint64 down, quint64 psi) {
     nextPSY = psi;
     */
     tcpSend();
+}
+
+// this function is an extension of TcpRov::tcpConnect and was made because "connect" is a reserved word for QT. But this function does not inherit from QT.
+void winsockConnect(SOCKET *_ConnectSocket, addrinfo *ptr) {
+    // Connect to server.
+    int iResult = connect(*_ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        closesocket(*_ConnectSocket);
+        *_ConnectSocket = INVALID_SOCKET;
+    }
+
+    if (*_ConnectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+    }
 }
--- tcprov.h
+++ tcprov.h
@@ -2,10 +2,8 @@
 #define TCPROV_H
 
 
-//#include <winsock2.h>
-
 #include <QObject>
-
+#include <winsock2.h>
 
 class TcpRov : public QObject
 {
@@ -21,7 +19,14 @@ public:
     double tStp = 0.1;						// Communication interval
     double recNum = 4;						// Number of messages (8-byte double precision numbers) to receive
     double senNum = 4;						// Number of messages (8-byte double precision numbers) to send
-//    WSADATA wsaData;                        // initalize Winsock
+
+    WSADATA wsaData;                        // initalize Winsock
+
+    std::string IPString = "127.0.0.1";		// IP address of FhSim TCP server
+    std::string PortString = "5000";		// Port number of FhSim TCP server
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+
 
 signals:
 
@@ -31,5 +36,7 @@ public slots:
     void tcpSend();
     void setValues(quint64, quint64, quint64, quint64);
 };
+
+void winsockConnect(SOCKET *, addrinfo *);
 
 #endif // TCPROV_H
